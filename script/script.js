ce26c0406c39c49a90751c03419296b78a5668da
Vaibhaw <kvaibhaw99@gmail.com>
2021-12-28 11:25:00 -0500

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -66,3 +66,32 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+
+
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+
+  if (!clicked) return;
+
+  // Remove active classes
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+
+
