9a7d156da7ad87309c198b2d0674ae2f62ed0c8d
Vaibhaw <kvaibhaw99@gmail.com>
2021-12-29 11:25:00 -0500

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -94,4 +94,18 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 
+// Menu fade away animation
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
 
