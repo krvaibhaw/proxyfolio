0aab0b1666aacf11a80c1e41bae8ffb125b56e57
Vaibhaw <kvaibhaw99@gmail.com>
2022-01-03 11:25:00 -0500

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -109,3 +109,32 @@ const handleHover = function (e) {
   }
 };
 
+// Passing "argument" into handler
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+
+
