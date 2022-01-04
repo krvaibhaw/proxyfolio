e5a719caff1483c9e3b62d882d9636362bce09c1
Vaibhaw <kvaibhaw99@gmail.com>
2022-01-04 11:25:00 -0500

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -138,3 +138,26 @@ headerObserver.observe(header);
 
 
 
+// Reveal sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
