6f59ff2348ad79aaf2125c1409453d1f1bc5ba19
Vaibhaw <kvaibhaw99@gmail.com>
2022-01-07 11:25:00 -0500

    Update JavaScript

--- script/script.js
+++ script/script.js
@@ -161,3 +161,34 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+
+//  Slow loading image effect
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replacing src with data-src
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
+
